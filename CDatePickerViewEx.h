0a63ef56e8a4e3bb0e7b7dc9115c68f73fff463b
Adrian Somesan <adrian.somesan@3pillarglobal.com>
2012-12-10 22:06:11 +0200

    financial info: added start date personal info: added former address view if current living years < 2 years

---
+++ CDatePickerViewEx.h
@@ -0,0 +1,13 @@
+//
+//  CDatePickerViewEx.h
+//  TD Visa Form
+//
+//  Created by Adrian Somesan on 12/10/12.
+//  Copyright (c) 2012 Adrian Somesan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDatePickerViewEx : UIPickerView
+
+@end
---
+++ CDatePickerViewEx.m
@@ -0,0 +1,31 @@
+//
+//  CDatePickerViewEx.m
+//  TD Visa Form
+//
+//  Created by Adrian Somesan on 12/10/12.
+//  Copyright (c) 2012 Adrian Somesan. All rights reserved.
+//
+
+#import "CDatePickerViewEx.h"
+
+@implementation CDatePickerViewEx
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
