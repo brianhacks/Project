ce0380cbc2c0d953d894a48fd5ea210d47e69439
Matt Kantor <matt@littlekitchen.ca>
2012-12-12 13:14:20 -0500

    changed graphics for headers fixed AD

--- TD Visa Form/AppDelegate.m
+++ TD Visa Form/AppDelegate.m
@@ -84,8 +84,8 @@
     
 
 //     self.window.rootViewController = self.thankYouViewController;
-   // self.window.rootViewController = self.firstScreenSaverViewController;
-   self.window.rootViewController = self.personalInfoViewController;
+    self.window.rootViewController = self.firstScreenSaverViewController;
+   //self.window.rootViewController = self.personalInfoViewController;
     
     
     [self.window makeKeyAndVisible];
